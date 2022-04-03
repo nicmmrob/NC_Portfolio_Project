102bd98c8cfa89ce1d32b5b88eb2b05bacb5b3c2
Nic Robbins <monicamfair@gmail.com>
2022-04-03 16:04:23 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+app = Flask(__name__)
+import os
+from flask import Flask
+from flask_migrate import Migrate
+
+# https://flask.palletsprojects.com/en/2.0.x/patterns/appfactories/
+
+
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello! Welcome to my portfolio project, "Plant Shop" - Utilizing Docker in VS Code with Flask!'
+
+
+
+def index_customers():
+    return 'This is the page to index existing customers or create new ones.'
+app.add_url_rule('/get-customers', 'index_customers', index_customers)
+
+def index_plants():
+    return 'This is the page to index existing plants or create new ones.'
+app.add_url_rule('/get-plants', 'index_plants', index_plants)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
